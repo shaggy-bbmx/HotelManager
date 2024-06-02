c3cb5af6e08fab31c4199deb35637bb3230f428d
sagar_patel <sagar.bcpatel@gmail.com>
2024-06-02 19:45:56 +0530

    socket related changes-4

--- server/src/main/java/hotelManager/server/Config/WebSocketConfig.java
+++ server/src/main/java/hotelManager/server/Config/WebSocketConfig.java
@@ -15,13 +15,13 @@ public class WebSocketConfig {
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-         config.setHostname("0.0.0.0");
+        config.setHostname("0.0.0.0");
 
         config.setPort(Integer.parseInt(socketPort));
 
         //For Dev mode choose ==1))
-        //config.setOrigin("http://localhost:4000");
-        config.setOrigin("https://hotelmanager-q6bz.onrender.com");
+//        config.setOrigin("http://localhost:4000");
+       config.setOrigin("https://hotelmanager-q6bz.onrender.com:4000");
         return new SocketIOServer(config);
     }
 
